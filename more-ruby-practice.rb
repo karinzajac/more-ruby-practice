0fd9dbed5cfe8db626b9d4e454009856641d198c
Karin <kczajac@gmail.com>
2016-09-25 12:09:29 -0400

    Ive added a comment

--- more-ruby-practice.rb
+++ more-ruby-practice.rb
@@ -8,7 +8,8 @@ thing_two = gets.strip
 
 puts ("What is a third favorite thing?")
 thing_three	= gets.strip
-
 # binding.pry
-
 puts ("These are a few of my favorite things: #{thing_one}, #{thing_two}, #{thing_three}")
+
+
+# this is a comment
